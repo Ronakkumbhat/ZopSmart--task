b2d196428f4c754c67712f0f76ae4ad3d9f4ef1f
Ronak Kumbhat <ronakkumbhat8@gmail.com>
2023-12-09 21:57:00 +0530

    basic post req and local storage

--- main.go
+++ main.go
@@ -1,35 +1,77 @@
 package main
 
 import (
+	"strconv"
     "encoding/json"
     "fmt"
     "gofr.dev/pkg/gofr"
 )
 
 func main() {
-    // initialise gofr object
-    app := gofr.New()
+	// initialise gofr object
+	app := gofr.New()
+	var books []string
 
-    // register route greet
-    app.GET("/greet", func(ctx *gofr.Context) (interface{}, error) {
-        // Get data from request body
-        var requestData struct {
-            Name string `json:"name"`
-        }
-    
-        // Use json.NewDecoder to decode the request body into requestData
-        err := json.NewDecoder(ctx.Request().Body).Decode(&requestData)
-        if err != nil {
-            return nil, err
-        }
-    
-        // Access the data
-        fmt.Println("Hello", requestData.Name)
+	// register route greet
+	app.GET("/addbook", func(ctx *gofr.Context) (interface{}, error) {
+		// Get data from request body
+		var requestData struct {
+			Name string `json:"Name"`
+		}
 
-        return "Hello " + requestData.Name
-    })
+		// Use json.NewDecoder to decode the request body into requestData
+		err := json.NewDecoder(ctx.Request().Body).Decode(&requestData)
+		if err != nil {
+			return nil, err
+		}
 
-    // Starts the server, it will listen on the default port 8000.
-    // it can be over-ridden through configs
-    app.Start()
+		// Insert the value into the array
+		books = append(books, requestData.Name)
+		// Access the data
+		fmt.Println("Hello", books)
+
+		// Get the last element of the slice
+		lastBook := books[len(books)-1]
+
+		return "Hello " + lastBook,nil
+	})
+	app.GET("/updatestatus", func(ctx *gofr.Context) (interface{}, error) {
+		// Get data from request body
+		var requestData struct {
+			Name string `json:"Name"`
+			Status string `json:"Status"`
+		}
+
+		// Use json.NewDecoder to decode the request body into requestData
+		err := json.NewDecoder(ctx.Request().Body).Decode(&requestData)
+		if err != nil {
+			return nil, err
+		}
+
+		var tempbooks []string
+		if(requestData.Status == "returned"){
+			books = append(books, requestData.Name)
+		}else if(requestData.Status == "borrowed"){
+			for i := 0; i < len(books); i++ {
+				if(books[i] != requestData.Name){
+					tempbooks = append(tempbooks, books[i])
+				}
+			}
+			books = tempbooks
+		}else if(requestData.Status == "lost"){
+			for i := 0; i < len(books); i++ {
+				if(books[i] != requestData.Name){
+					tempbooks = append(tempbooks, books[i])
+				}
+			}
+			books = tempbooks
+		}else{
+			fmt.Println("invalid", books, requestData)
+		
+		}
+		return "Hello " + strconv.Itoa(len(books)),nil
+	})
+	// Starts the server, it will listen on the default port 8000.
+	// it can be over-ridden through configs
+	app.Start()
 }
